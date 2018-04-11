65b34acada7cb44a9cbc5729e6205ff7fded8a0e
John Barlow <john.barlow@uvm.edu>
2018-04-11 14:40:56 -0400

    functions code from class

---
+++ MyFunctions.R
@@ -0,0 +1,38 @@
+# FUNCTION: GetData
+# input: x
+#Ouput: y
+#--------------------------------------------------------
+
+GetData <- function() { return("testing........ GetData")}
+
+#############################################################
+
+
+# FUNCTION:  CalculateStuff
+# input: x
+#Ouput: y
+#--------------------------------------------------------
+
+CalculateStuff <- function() { return("testing........ CalculateStuff")}
+
+#############################################################
+
+# FUNCTION: SummarizeOutput
+# input: x
+#Ouput: y
+#--------------------------------------------------------
+
+SummarizeOutput <- function() { return("testing........ SummarizeOutput")}
+
+############################################################
+
+# FUNCTION: GraphResults
+# input: x
+#Ouput: y
+#--------------------------------------------------------
+
+GraphResults <- function() { return("testing........ GraphResults")}
+
+#########################################################
+
+install.packages("lubridate")
