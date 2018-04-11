d3d737a4a8cdeaef0e2ed54bc6238ee82cc567dd
John Barlow <john.barlow@uvm.edu>
2018-04-11 14:34:13 -0400

    functions code from class

---
+++ Functions get data mar20.R
@@ -0,0 +1,11 @@
+#########################################################
+
+#FUNCTION: GetData
+# one line description 
+# input: x
+# output: y
+#-------------------------------------------------------
+
+# this lecture is about control struture 
+
+GetData <- 
